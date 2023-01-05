ade94d37293101816fd5a5a309a7d5509b243615
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-01-05 19:59:02 +0530

    Add files via upload

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -21,6 +21,7 @@ var input = document.getElementById('input-area')
 
 var inputString = ''
 document.body.addEventListener('keypress', function(event) {
+    alert("clicked")
     var key = event.key
     
     if (key === '1'|| key=='2'|| key=='3'|| key=='4'|| key=='5'|| key=='6'|| key=='7'|| key=='8'|| key=='9'|| key=='0') {
@@ -30,25 +31,106 @@ document.body.addEventListener('keypress', function(event) {
     else if(key==='+'){
       inputString+=key
       input.innerText=inputString
- }
- else if(key==='-'){
-    inputString+=key
+    }
+    else if(key==='-'){
+        inputString+=key
+        input.innerText=inputString
+    }
+    else if(key==='*'){
+        inputString+=key
+        input.innerText=inputString
+    }
+    else if(key==='/'){
+        inputString+=key
+        input.innerText=inputString
+    }
+    else if(key==='c'|| key==='C'){
+        inputString=''
+        input.innerText=inputString
+    }
+    else if(key==='='|| key==='Enter'){
+        inputString=eval(inputString)
+        input.innerText=inputString
+    }
+})
+
+zero.addEventListener('click',function(){
+    inputString+='0'
     input.innerText=inputString
-}
-else if(key==='*'){
-    inputString+=key
+})
+
+one.addEventListener('click',function(){
+    inputString+='1'
     input.innerText=inputString
-}
-else if(key==='/'){
-    inputString+=key
+   })
+
+
+two.addEventListener('click',function(){
+    inputString+='2'
     input.innerText=inputString
-}
-else if(key==='c'|| key==='C'){
+})   
+
+three.addEventListener('click',function(){
+    inputString+='3'
+    input.innerText=inputString
+   })
+
+four.addEventListener('click',function(){
+    inputString+='4'
+    input.innerText=inputString
+})   
+
+five.addEventListener('click',function(){
+    inputString+='5'
+    input.innerText=inputString
+})
+
+six.addEventListener('click',function(){
+    inputString+='6'
+    input.innerText=inputString
+   })
+
+seven.addEventListener('click',function(){
+    inputString+='7'
+    input.innerText=inputString
+})
+
+eight.addEventListener('click',function(){
+    inputString+='8'
+    input.innerText=inputString
+   })
+
+nine.addEventListener('click',function(){
+    inputString+='9'
+    input.innerText=inputString
+})
+
+clear.addEventListener('click',function(){
     inputString=''
     input.innerText=inputString
-}
-else if(key==='='|| key==='Enter'){
+   })
+
+divide.addEventListener('click',function(){
+    inputString+='/'
+    input.innerText=inputString
+})
+
+add.addEventListener('click',function(){
+    inputString+='+'
+    input.innerText=inputString
+})   
+
+subtract.addEventListener('click',function(){
+    inputString+='-'
+    input.innerText=inputString
+})
+
+multiply.addEventListener('click',function(){
+    inputString+='*'
+    input.innerText=inputString
+})   
+
+equalTo.addEventListener('click',function(){
     inputString=eval(inputString)
     input.innerText=inputString
-}
 })
